8d66b14c749d9ade588ca084222a9f6e1fed2f31
Cons Golosino <consorcio.golosino@avensia.com>
2025-10-03 15:02:37 +0800

    enhanced UI Part 1
    enhanced UI Part 1

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,27 +3,54 @@
 import React, { useActionState } from 'react';
 import { Input } from '@/components/ui/input';
 import { register } from '@/app/lib/auth';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Register = () => {
   const [state, action] = useActionState(register, undefined);
   return (
-    <div>
-      <form action={action}>
-        <Input name="email" type="text" placeholder="Email" defaultValue={state?.email} />
-        <div>{state?.errors?.email && <p>{state.errors.email}</p>}</div>
-        <Input name="password" type="password" placeholder="Password" />
-        {state?.errors?.password && (
-          <div>
-            <div className="h-4 w-4" />
-            <div>Password must:</div>
-            {Array.isArray(state.errors.password) &&
-              state.errors.password.map((error, index) => <div key={index}>{error}</div>)}
+    <div className="flex items-center justify-center pt-50">
+      <div className="w-100 border border-gray-400 rounded-md p-5 shadow-xl">
+        <div className="flex justify-center mb-5">
+          <Image
+            className="object-cover w-50"
+            alt="Avensia Logo"
+            src="/assets/images/avensia-horizontal-dark.png"
+            width={350}
+            height={97}
+          />
+        </div>
+        <form action={action}>
+          <div className="mb-5">
+            <Label className="mb-2" htmlFor="email">
+              Email
+            </Label>
+            <Input id="email" type="text" name="email" placeholder="Email" defaultValue={state?.email} />
           </div>
-        )}
-        <Input name="confirmPassword" type="password" placeholder="Confirm Password" />
-        <div>{state?.errors?.confirmPassword && <p>{state.errors.confirmPassword}</p>}</div>
-        <button type="submit">Register</button>
-      </form>
+          <div className="mb-5">
+            <Label className="mb-2" htmlFor="password">
+              Password
+            </Label>
+            <Input id="password" type="password" name="password" placeholder="Password" />
+          </div>
+          <div className="mb-5">
+            <Label className="mb-2" htmlFor="confirmpassword">
+              Confirm Password
+            </Label>
+            <Input id="confirmpassword" type="password" name="confirmpassword" placeholder="Confirm Password" />
+          </div>
+          <div className="mb-5">
+            {state?.errors?.email && <p className="text-red-600">{state.errors.email}</p>}
+            {state?.errors?.password && <p className="text-red-600">{state.errors.password}</p>}
+          </div>
+          <div className="flex items-center justify-between">
+            <Button type="submit">Register</Button>
+            <Link href="/login">Login</Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/app/(page)/layout.tsx
+++ src/app/(page)/layout.tsx
@@ -1,3 +1,5 @@
+import { PageCenter } from '@/components/ui/box';
+import Image from 'next/image';
 import React from 'react';
 
 type PropsType = {
@@ -5,7 +7,22 @@ type PropsType = {
 };
 
 const PageLayout = ({ children }: Readonly<PropsType>) => {
-  return <div className="mt-10 mx-auto">{children}</div>;
+  return (
+    <PageCenter>
+      <div className="min-h-screen w-full bg-gray-50 py-10">
+        <div className="mb-10">
+          <Image
+            className="object-cover w-50"
+            alt="Avensia Logo"
+            src="/assets/images/avensia-horizontal-dark.png"
+            width={350}
+            height={97}
+          />
+        </div>
+        {children}
+      </div>
+    </PageCenter>
+  );
 };
 
 export default PageLayout;
