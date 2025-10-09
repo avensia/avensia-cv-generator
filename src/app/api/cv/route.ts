19ca33b303a483a7c4b668f38b6c2d3b334a07c2
Vince Xavier C. del Rosario <112623158+avensia-vince-delrosario@users.noreply.github.com>
2025-10-09 09:59:36 +0800

    Modify GET cv endpoint to acquire relevant documents related to the user

--- src/app/api/cv/route.ts
+++ src/app/api/cv/route.ts
@@ -28,21 +28,24 @@ export async function POST(req: Request) {
   }
 }
 
-export async function GET(req: Request) {
+export async function GET(req: Request) { 
   const user = await getAuthUser();
   if (!user) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
 
   try {
-    const col = await getCollection<CvData & { createdAt: Date; updatedAt: Date; userId: ObjectId }>('cvs');
+    const cvCol = await getCollection<CvData & { createdAt: Date; updatedAt: Date; userId: ObjectId }>('cvs');
+    const eduCol = await getCollection<{ createdAt: Date; updatedAt: Date; user_id: ObjectId }>('educations');
+    const workExCol = await getCollection<{ createdAt: Date; updatedAt: Date; user_id: ObjectId }>('workexperiences');
+    const projCol = await getCollection<{ createdAt: Date; updatedAt: Date; user_id: ObjectId }>('projects');
 
     // Check if an id param was provided
     const { searchParams } = new URL(req.url);
     const id = searchParams.get('id');
 
     if (id) {
-      const doc = await col.findOne({ _id: new ObjectId(id), userId: new ObjectId(user.userId) });
+      const doc = await cvCol.findOne({ _id: new ObjectId(id), userId: new ObjectId(user.userId) });
       if (!doc) {
         return NextResponse.json({ error: 'CV not found or not owned by user' }, { status: 404 });
       }
@@ -50,7 +53,7 @@ export async function GET(req: Request) {
     }
 
     // Otherwise: just get the single CV for this user (most recent one if multiple exist)
-    const doc = await col.findOne(
+    const doc = await cvCol.findOne(
       { userId: new ObjectId(user.userId) },
       {
         sort: { createdAt: -1 },
@@ -60,8 +63,24 @@ export async function GET(req: Request) {
     if (!doc) {
       return NextResponse.json({ error: 'No CV found for this user' }, { status: 404 });
     }
+    
+    // Fetch all related educations to the user
+    const educationDocs = await eduCol.find({ user_id: new ObjectId(user.userId) }).sort({ createdAt: -1 }).toArray();
+    
+    // Fetch all related projects to the user
+    const workExperienceDocs = await workExCol.find({ user_id: new ObjectId(user.userId) }).sort({ createdAt: -1 }).toArray();
+    
+    // Fetch all related educations to the user
+    const projectDocs = await projCol.find({ user_id: new ObjectId(user.userId) }).sort({ createdAt: -1 }).toArray();
 
-    return NextResponse.json(doc, { status: 200 });
+    const response = {
+      ...doc,
+      projects : projectDocs,
+      workExperience : workExperienceDocs,
+      educations : educationDocs,
+    }
+
+    return NextResponse.json(response, { status: 200 });
   } catch (e) {
     console.error(e);
     return NextResponse.json({ error: 'Failed to fetch CV' }, { status: 500 });
